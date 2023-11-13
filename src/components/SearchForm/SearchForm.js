e304acc51b2d1df7afc4d910f123e623f72aecaa
maria <mdmolokova@edu.hse.ru>
2023-11-14 00:44:39 +0300

    Added focus to search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,23 @@
+import { React, useState } from "react";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
 import "./SearchForm.css";
 
 function SearchForm() {
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsInputFocused(true);
+  }
+
+  const handleBlur = () => {
+    setIsInputFocused(false);
+  }
+
   return (
     <section className="search-form">
-      <div className="search-form__fields-container">
-        <form className="search-form__form">
+      <div className={`search-form__fields-container ${isInputFocused ? "search-form__fields-container_focused" : ""}`}>
+        <form className={`search-form__form ${isInputFocused ? "search-form__form_focused" : ""}`}>
         <div className="search-form__icon" />
           <input
             className="search-form__input"
@@ -14,6 +25,8 @@ function SearchForm() {
             type="text"
             placeholder="Фильм"
             required
+            onFocus={handleFocus}
+            onBlur={handleBlur}
           />
           <button
             className="search-form__submit-button active-button"
